7bff5bf6f768cfdd693b690ea3651e0d6dde4991
auful01 <distriq19@gmail.com>
2022-04-08 05:19:29 +0700

    update@styling

--- resources/views/layout/topbar.blade.php
+++ resources/views/layout/topbar.blade.php
@@ -111,7 +111,7 @@
                 </h6>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="img/undraw_profile_1.svg" alt="...">
+                        <img class="rounded-circle" src="{{asset('img/undraw_profile_1.svg')}}" alt="...">
                         <div class="status-indicator bg-success"></div>
                     </div>
                     <div class="font-weight-bold">
@@ -122,7 +122,7 @@
                 </a>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="img/undraw_profile_2.svg" alt="...">
+                        <img class="rounded-circle" src="{{asset('img/undraw_profile_2.svg')}}" alt="...">
                         <div class="status-indicator"></div>
                     </div>
                     <div>
@@ -133,7 +133,7 @@
                 </a>
                 <a class="dropdown-item d-flex align-items-center" href="#">
                     <div class="dropdown-list-image mr-3">
-                        <img class="rounded-circle" src="img/undraw_profile_3.svg" alt="...">
+                        <img class="rounded-circle" src="{{asset('img/undraw_profile_3.svg')}}" alt="...">
                         <div class="status-indicator bg-warning"></div>
                     </div>
                     <div>
@@ -165,7 +165,7 @@
                 aria-haspopup="true" aria-expanded="false">
                 <span
                     class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->nama_depan . ' ' . Auth::user()->nama_belakang }}</span>
-                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
+                <img class="img-profile rounded-circle" src="{{asset('img/undraw_profile.svg')}}">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
