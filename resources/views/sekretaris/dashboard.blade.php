1fc0d0dbfaf67e85eaa8ff87cf1e2d40056060c2
auful01 <distriq19@gmail.com>
2022-06-08 00:47:15 +0700

    Rev5

--- resources/views/sekretaris/dashboard.blade.php
+++ resources/views/sekretaris/dashboard.blade.php
@@ -66,7 +66,7 @@
                 <div class="icon">
                     <i class="fas fa-clipboard-list " ></i>
                 </div>
-                {{-- <a href="{{route('data-anggota')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> --}}
+                <a href="{{route('data-anggota')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <div class="col-lg-3 col-6">
